42b5405d2620c249294e62481fef4020ed32f927
Sparklegift <godsgiftetuk2001@gmail.com>
2023-05-09 23:05:49 -0700

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte.
+ * @s: source string
+ * @accept: searching string
+ * Return: new string.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (j = 0; *(s + j); j++)
+	{
+
+		for (i = 0; *(accept + i); i++)
+		{
+			break;
+		}
+	if (*(accept + i) == '\0')
+		break;
+	}
+	return (j);
+}
