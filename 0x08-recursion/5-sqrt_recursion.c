e8ff5ded5edee6db730e39470b32bef2d62def40
Sparklegift <godsgiftetuk2001@gmail.com>
2023-05-10 20:18:39 -0700

    fixing

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,32 @@
 #include "main.h"
 /**
+ * sqrthelper - If n does not have a natural square root
+ * @num: number
+ * @sqrtn: square root of num
+ * Return: -1
+ */
+int sqrthelper(int num, int sqrtn)
+{
+	if ((sqrtn * sqrtn) == num)
+	{
+		return (sqrtn);
+	}else
+	{
+		if ((sqrtn * sqrtn) > num)
+			return (-1);
+		else
+			return (sqrthelper(num, sqrtn + 1));
+	}
+}
+/**
  * _sqrt_recursion - the natural square root of a number.
- *
+ * @n: number to find sqrt.
+ * Return: square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (sqrthelper(n, 0));
+}
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,2 +1,41 @@
 #include "main.h"
 /**
+ * primehelper - returns 0 or 1
+ * @num: mumber
+ * @i: factor of num
+ * Return: 0 if not prime, 1 if prime
+ */
+int primehelper(int num, int i)
+{
+	if (i < num)
+	{
+		if (num % i == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (primehelper(num, i + 1));
+		}
+	}
+	else
+	{
+		return (1);
+	}
+}
+/**
+ * is_prime_number - if number is prime or not
+ * @n: number
+ * Return: 1 if number is prime
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (primehelper(n, 2));
+	}
+}
