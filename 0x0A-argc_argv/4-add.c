b4b4d0dc249266460919bcf280a3b5fa6577e325
Sparklegift <godsgiftetuk2001@gmail.com>
2023-05-15 22:48:24 -0700

    Even advance task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * main - adds positive numbers.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, i;
+	if (argc > 1)
+	{
+
+		for (i = 1; i < argc; i++)
+		{
+			int k;
+			char *str;
+
+
+			str = argv[i];
+			for (k = 0; str[k] != "\0"; k++)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
