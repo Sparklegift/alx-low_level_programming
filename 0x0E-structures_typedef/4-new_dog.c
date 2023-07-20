61a5ab1da6e349cb381f2f384220e4a1e1ce3902
Sparklegift <129224621+Sparklegift@users.noreply.github.com>
2023-07-20 14:59:26 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,37 +11,32 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int i leg, len;
 	
 	struct dog *n_dog = NULL;
-
+	
 	leg = 0;
-
-while (name[leg] != '\0')
-leg++;
-
-Leno = 0;
-
-while (owner[len] != '\0')
-len++;
-
-n_dog = malloc(sizeof (struct dog);
-if (n_dog == NULL)
-{
-free(n_dog);
-return (NULL);
-}
-n_dog->name = malloc (leg + 1);
-if (n_dog->name == NULL)
-{
-Free(n_dog->nane);
-free(n_dog->owner);
-free(n_dog);
-return (NULL);
-}
-for (i = 0; i <= leg; i++)
-
-n_dog->name[i] = name[i];
-
-for (i = 0; i <= len; i++)
-	n_dog->owner[i] = owner[i];
+	while (name[leg] != '\0')
+		leg++;
+	len = 0;
+	while (owner[len] != '\0')
+		len++;
+	
+	n_dog = malloc(sizeof (struct dog);
+	if (n_dog == NULL)
+	{
+		free(n_dog);
+		return (NULL);
+	}
+	n_dog->name = malloc (leg + 1);
+	if (n_dog->name == NULL)
+	{
+		free(n_dog->nane);
+		free(n_dog->owner);
+		free(n_dog);
+		return (NULL);
+	}
+	for (i = 0; i <= leg; i++)
+		n_dog->name[i] = name[i];
+	for (i = 0; i <= len; i++)
+		n_dog->owner[i] = owner[i];
 	n_dog->age = age;
-	return (n_dog)
+	return (n_dog);
 }
