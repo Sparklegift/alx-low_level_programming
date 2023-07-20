bcd4f946ef04b82882018f636587d3dda21c3ec2
Sparklegift <godsgiftetuk2001@gmail.com>
2023-07-20 06:43:12 -0700

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - creates a new dog.
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i leg, len;
+	
+	struct dog *n_dog = NULL;
+
+	Tenn = 0;
+
+while (name[lent] = *\o')
+Tentes;
+
+Leno = 0;
+
+while (ouner[leno] I= ‘\o')
+Lenows;
+
+n_dog = malloc(sizeof (struct dog);
+if (n_dog == nuLL)
+free(n_dog);
+return (WL);
+}
+n_dog->name = malloc (lent + 1);
+if (n_dog->name == LL)
+«
+
+Free(n_dog-onane);
+free(n_dog);
+return (Wi);
+for (1= 05 1 <= Ten; 144)
+
+n_dog->name[i] = name[i];
+
+for (i = 0; i <= leno; i++)
+}
