f7bcd4962a6192a31284792ba1f6f9ed3b3765db
Sparklegift <godsgiftetuk2001@gmail.com>
2023-07-20 03:24:45 -0700

    Trial

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,24 @@
+#ifndef STRCTYP
+#define STRCTYP
+
+/**
+ * struct dog - structure of a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner of dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+typedef struct dog dog_str;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
