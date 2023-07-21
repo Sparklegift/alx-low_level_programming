aff2a20e2ef369e074c4b1ddceacccbbe2944b99
Sparklegift <129224621+Sparklegift@users.noreply.github.com>
2023-07-21 20:13:50 +0100

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,33 +3,32 @@
  * int_index - searches for an integer
  * @array: integers array
  * @size: elements in the array
- * @cmp: pointer to fuction
- * Return: index of element
+ * @cmp: pointer to function that compares the integer
+ * Return: index of element or -1 if not found or in case of error
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, resu;
+    int i, resu;
 
-	resu = -1;
-	if (array && cmp)
-	{
-		if (size <= 0)
-		{
-			return (resu);
-		}
-		for (i = 0; i < size; i++)
-		{
-			cmp(array[i]);
-			if(cmp(array[i]) > 0)
-			{
-				resu = i;
-				break;
-			}
-			if ((cmp(array[i]) -- (-1)))
-			{
-				return (resu);
-			}
-		}
-	}
+    resu = -1;
+    if (array && cmp)
+    {
+        if (size <= 0)
+        {
+            return resu; // Return -1 if the size is invalid (non-positive)
+        }
+        for (i = 0; i < size; i++)
+        {
+            if (cmp(array[i]) > 0)
+            {
+                resu = i;
+                break;
+            }
+            else if (cmp(array[i]) < 0)
+            {
+                return resu; // Return -1 if there is an error in the comparison
+            }
+        }
+    }
+    return resu; // Return -1 if no element meets the comparison condition
 }
-
