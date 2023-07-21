cb4b38047b94696f91f3aa67d35456226cb8f48c
Sparklegift <godsgiftetuk2001@gmail.com>
2023-07-21 12:07:58 -0700

    going well

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+/**
+ * int_index - searches for an integer
+ * @array: integers array
+ * @size: elements in the array
+ * @cmp: pointer to fuction
+ * Return: index of element
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, resu;
+
+	resu = -1;
+	if (array && cmp)
+	{
+		if (size <= 0)
+		{
+			return (resu);
+		}
+		for (i = 0; i < size; i++)
+		{
+			cmp(array[i]);
+			if(cmp(array[i]) > 0)
+			{
+				resu = i;
+				break;
+			}
+			if ((cmp(array[i]) -- (-1)))
+			{
+				return (resu);
+			}
+		}
+	}
+}
+
