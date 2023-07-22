5405299176343a136daceec2881497b61924af29
Sparklegift <129224621+Sparklegift@users.noreply.github.com>
2023-07-22 13:48:47 +0100

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 /**
  * int_index - searches for an integer
  * @array: integers array
@@ -9,7 +10,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
     int i;
 
-    if ((array && cmp) && size <= 0)
+    if ((array == NULL || cmp == NULL) || size <= 0)
     {
         return (-1);
     }
