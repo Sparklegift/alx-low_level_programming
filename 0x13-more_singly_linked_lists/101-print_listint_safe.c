70fe524c5c7f0db74a712bf8d1537822685c53c5
Sparklegift <129224621+Sparklegift@users.noreply.github.com>
2023-08-06 02:36:00 +0100

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,47 +6,44 @@ size_t print_listint_safe(const listint_t *head);
 
 /**
  * looped_listint_len - Counts the number of unique nodes
- * in a looped listint_t linked list.
  * @head: A pointer to the head of the listint_t to check.
- *
- * Return: If the list is not looped - 0.
- * Otherwise - the number of unique nodes in the list.
+ * Return: If the list is not looped
  */
 size_t looped_listint_len(const listint_t *head)
 {
-	const listint_t *tortoise, *hare;
+	const listint_t *i, *j;
 	size_t nodes = 1;
 
 	if (head == NULL || head->next == NULL)
 		return (0);
 
-	tortoise = head->next;
-	hare = (head->next)->next;
+	i = head->next;
+	j = (head->next)->next;
 
-	while (hare)
+	while (j)
 	{
-		if (tortoise == hare)
+		if (i == j)
 		{
-			tortoise = head;
-			while (tortoise != hare)
+			i = j;
+			while (i != j)
 			{
 				nodes++;
-				tortoise = tortoise->next;
-				hare = hare->next;
+				i = i->next;
+				j = j->next;
 			}
 
-			tortoise = tortoise->next;
-			while (tortoise != hare)
+			i = i->next;
+			while (i != j)
 			{
 				nodes++;
-				tortoise = tortoise->next;
+				i = i->next;
 			}
 
 			return (nodes);
 		}
 
-		tortoise = tortoise->next;
-		hare = (hare->next)->next;
+		i = i->next;
+		j = (j->next)->next;
 	}
 
 	return (0);
@@ -55,7 +52,6 @@ size_t looped_listint_len(const listint_t *head)
 /**
  * print_listint_safe - Prints a listint_t list safely.
  * @head: A pointer to the head of the listint_t list.
- *
  * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
@@ -86,4 +82,3 @@ size_t print_listint_safe(const listint_t *head)
 
 	return (nodes);
 }
-
