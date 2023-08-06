ed74dfaba78915a05f067fb9f64d672cabaa6dbb
Sparklegift <129224621+Sparklegift@users.noreply.github.com>
2023-08-06 02:19:52 +0100

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,8 +2,7 @@
 
 /**
  * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
- *
+ * @h: linked list
  * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
@@ -18,4 +17,3 @@ size_t listint_len(const listint_t *h)
 
 	return (num);
 }
-
