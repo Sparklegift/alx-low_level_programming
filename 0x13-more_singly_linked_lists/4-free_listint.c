4abc304e1756b5c8eef58753288660a443f43bf5
Sparklegift <129224621+Sparklegift@users.noreply.github.com>
2023-08-06 02:22:33 +0100

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,13 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
+	listint_t *tem;
 
 	while (head)
 	{
-		temp = head->next;
+		tem = head->next;
 		free(head);
-		head = temp;
+		head = tem;
 	}
 }
-
